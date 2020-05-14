20b7213341bafbd4338659bb7841c7f759dd1338
Kin Ueng <github@kinueng.com>
2020-05-14 10:32:14 -0500

    Remove submenu for applications in left side menu

--- src-web-v2/components/LeftNavMenu.js
+++ src-web-v2/components/LeftNavMenu.js
@@ -1,9 +1,6 @@
 import React, { PureComponent } from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-require('./LeftNavMenu.scss');
-
-import msgs from '../../nls/kappnav.properties';
 
 import {
   SideNav,
@@ -20,22 +17,23 @@ import {
   Category20,
   TreeViewAlt20,
 } from '@carbon/icons-react';
+import msgs from '../../nls/kappnav.properties';
+
+require('./LeftNavMenu.scss');
 
 export class LeftNavMenu extends PureComponent {
   render() {
     const { expanded } = this.props;
     return (
       <SideNav
-        aria-label="Side navigation"
+        aria-label={msgs.get('header.menu.bar.label')}
         isRail
         expanded={expanded}
       >
         <SideNavItems>
-          <SideNavMenu renderIcon={Application20} title={msgs.get('page.applicationView.title')}>
-            <SideNavMenuItem element={Link} to="/applications/stock-trader">
-              Stock Trader
-            </SideNavMenuItem>
-          </SideNavMenu>
+          <SideNavLink renderIcon={Application20} element={Link} to="/applications">
+            {msgs.get('page.applicationView.title')}
+          </SideNavLink>
           <SideNavLink renderIcon={Catalog20} element={Link} to="/jobs">
             {msgs.get('page.jobsView.title')}
           </SideNavLink>
