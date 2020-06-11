4c9713b66949ab7f2b7c9646e7fbe7bf28b9b1e9
Kin Ueng <github@kinueng.com>
2020-06-11 10:57:12 -0500

    redesign: Remove tab in favor of the left side nav

--- src-web-v2/containers/LandingPage.js
+++ src-web-v2/containers/LandingPage.js
@@ -2,78 +2,34 @@ import React from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
 
 import {
-  Tabs,
-  Tab,
-} from 'carbon-components-react';
-
-import {
   ApplicationTable,
   ActionHistoryTable,
 } from '../components';
 
-import msgs from '../../nls/kappnav.properties';
-import ActionHistoryResourceTable from '../components/tables/ActionHistoryTable';
-
 require('./LandingPage.scss');
 
-// This is used to avoid rendering tab content unless the tab is selected
-const TabContentRenderedOnlyWhenSelected = ({
-  selected,
-  children,
-  className,
-  ...other
-}) => (!selected ? (
-  <div {...other} className="bx--visually-hidden" />
-) : (
-  <div
-    {...other}
-    className="bx--tab-content"
-    selected={selected}
-  >
-    {children}
-  </div>
-));
-
 /**
- * @return {int} - the tab number corresponding to the URL
+ * @return {String} - get the resource type from URL
  */
-const getSelectedTab = (location) => {
+const getResourceTypeFromURL = (location) => {
   const path = location.pathname;
   const pathAsArray = path?.split('/')?.filter((indexItem) => indexItem != null);
   const lastDirectory = pathAsArray[pathAsArray?.length - 1];
-  switch (lastDirectory) {
-    case 'applications':
-      return 0;
-    case 'actions':
-      return 1;
-    default:
-      return 0;
-  }
-};
-
-const handleTabClick = (history, uri) => {
-  // FIXME: There is a possible browser back button infinite loop here
-  history.push(uri);
+  return lastDirectory;
 };
 
 const LandingPage = () => {
   const location = useLocation();
-  const history = useHistory();
-  return (
-    <Tabs className="kv--tabs" selected={getSelectedTab(location)}>
-      {/* FIXME: These tabs need to be dymanic based on Redux or something */}
-      <Tab
-        label={msgs.get('page.applicationView.title')}
-        renderContent={TabContentRenderedOnlyWhenSelected}
-        onClick={() => handleTabClick(history, 'applications')}
-      >
-        <ApplicationTable />
-      </Tab>
-      <Tab label={msgs.get('actions.history')} renderContent={TabContentRenderedOnlyWhenSelected} onClick={() => handleTabClick(history, 'actions')}>
-        <ActionHistoryTable />
-      </Tab>
-    </Tabs>
-  );
+  const resourcePage = getResourceTypeFromURL(location);
+  switch (resourcePage) {
+    case 'applications':
+      return <ApplicationTable />;
+    case 'actions':
+      return <ActionHistoryTable />;
+    default:
+      // FIXME: Maybe 404 page or redirect to homepage
+      return <ApplicationTable />;
+  }
 };
 
 export default LandingPage;
